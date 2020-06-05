a574e261422fdf532a64e27984df46bf76424666
Alex Wilkes <alexander.wilkes@cantab.net>
2020-06-05 20:14:54 +0100

    make it better

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def post_something():
 # A welcome message to test our server
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
+    return "<h1>Welcome to our server !! testing a change...</h1>"
 
 if __name__ == '__main__':
     # Threaded option to enable multiple instances for multiple user access support
