cb896e2a55c356ce4a6ca67f4423577b2ea12416
Alex Wilkes <alexander.wilkes@cantab.net>
2020-06-07 22:58:42 +0100

    Country detection by app. Each app only loads one model.

--- app.py
+++ app.py
@@ -22,10 +22,7 @@ app.config.from_object(Config)
 
 class PromptForm(FlaskForm):
     prompt = StringField("It works best when you give it a few letters to start with...")
-    submit_england = SubmitField('Name my English town!')
-    submit_us = SubmitField('Name my US town!')
-    submit_france = SubmitField('Name my French town!')
-    submit_germany = SubmitField('Name my German town!')
+    submit = SubmitField('Name my town!')
 
 
 def pad_left(prompt):
@@ -36,22 +33,17 @@ def pad_left(prompt):
         return ("1" * (3-len(prompt))) + prompt
 
 
-# Train the english town names model
-# english_town_names = fetch_list_england(english_towns_url)
-# english = TownLearner(english_town_names)
-# english.fit()
-#
-# us_town_names = fetch_data_britannica(us_towns_url)
-# us = TownLearner(us_town_names)
-# us.fit()
-#
-# france_town_names = fetch_data_britannica(france_towns_url)
-# france = TownLearner(france_town_names)
-# france.fit()
+# We must assign the right training list according to the ENV COUNTRY variable
+country_town_names_dict = {
+    "GERMANY": fetch_list_germany(germany_towns_url),
+    "ENGLISH": fetch_list_england(english_towns_url),
+    "US": fetch_data_britannica(us_towns_url),
+    "FRANCE": fetch_data_britannica(france_towns_url),
+}
 
-germany_town_names = fetch_list_germany(germany_towns_url)
-germany = TownLearner(germany_town_names)
-germany.fit()
+country = os.environ["COUNTRY"]
+model = TownLearner(country_town_names_dict[country])
+model.fit()
 
 
 # The only page we serve
@@ -59,19 +51,13 @@ germany.fit()
 def home():
     prompt = request.args.get('prompt')
 
-    if "submit_us" in request.args:
-        placeholder = us.generate_n_towns(pad_left(prompt))
-    elif "submit_england" in request.args:
-        placeholder = english.generate_n_towns(pad_left(prompt))
-    elif "submit_france" in request.args:
-        placeholder = france.generate_n_towns(pad_left(prompt))
-    elif "submit_germany" in request.args:
-        placeholder = germany.generate_n_towns(pad_left(prompt))
+    if prompt is not None:
+        placeholder = model.generate_n_towns(pad_left(prompt))
     else:
         placeholder = set()
 
     form = PromptForm()
-    return render_template('index.html', title='Name my town', form=form, placeholder=placeholder, country=os.environ["COUNTRY"])
+    return render_template('index.html', title='Name my town', form=form, placeholder=placeholder)
 
 
 if __name__ == '__main__':
